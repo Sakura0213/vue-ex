ba1203838bf8af5d51e8de3740dcbd54b5e344b7
zengxin0213 <zengxin@email.cn>
2023-08-11 09:45:30 +0800

    ceshi 1

--- src/main.js
+++ src/main.js
@@ -12,13 +12,19 @@ import globalReferences from "./components/globalReferences.vue";//引用一个�
 
 
 //createApp(App).mount('#app')  初始
-const app = createApp(App); //由根组件 App 创建一个实例
+const app = createApp(App); //由根组件 App 创建一个应用实例
 
 app.use(ElementPlus); //全局使用 element-plus
 
+
+//应用实例会暴露一个 .config 对象允许我们配置一些应用级的选项
+// app.config.errorHandler = (err) => {
+//   /* 处理错误 */
+// };
+
 app.component("globalReferences", globalReferences);//将模板注册为全局引用，在任何组件的模板中都可以引用 <globalReferences/>
 
-
 //将这个实例挂载到项目的 html 入口文件 index.html 的 id="app" 元素上，进行动态渲染
+//挂载应用实例之前完成所有应用配置！
 app.mount("#app");
 
