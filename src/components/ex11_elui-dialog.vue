9a41ca9b9f702fba0e531e0dff15628bf073685a
Sakura0213 <zengxin@email.cn>
2023-09-19 14:29:08 +0800

    1

--- src/components/ex11_elui-dialog.vue
+++ src/components/ex11_elui-dialog.vue
@@ -1,35 +1,9 @@
 <template>
   <BaseFooter title="11.测试elementui-dialog"></BaseFooter>
 
-  <h4 id="文件提示">文件提示</h4>
-  <ul>
-    <li>仅支持后缀为 .xls 与 .xlsx 的 excel 格式文件</li>
-  </ul>
-  <h4 id="工作表提示">工作表提示</h4>
-  <ul>
-    <li>
-      导入的工作表，其<font color="#F56600"> <strong>名称</strong> </font
-      >需要与系统中被导入<font color="#F56600">
-        <strong>模块的m&#39;c</strong> </font
-      >一致
-    </li>
-  </ul>
-  <h4 id="数据提示">数据提示</h4>
-  <ul>
-    <li>
-      <p>
-        工作表中导入的数据，其<font color="#F56600">
-          <strong>列头</strong> </font
-        >需要与系统中被导入模块的<font color="#F56600">
-          <strong>字段的名称</strong> </font
-        >一致
-      </p>
-      <ul>
-        <li>1</li>
-      </ul>
-    </li>
-  </ul>
-  <p>&nbsp;</p>
+<svg t="1695087576104" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="" p-id="2655" width="200" height="200"><path d="M160 32c-12 0-24.8 4.8-33.6 14.4S112 68 112 80v864c0 12 4.8 24.8 14.4 33.6 9.6 9.6 21.6 14.4 33.6 14.4h704c12 0 24.8-4.8 33.6-14.4 9.6-9.6 14.4-21.6 14.4-33.6V304L640 32H160z" fill="#5ACC9B" p-id="2656"></path><path d="M912 304H688c-12 0-24.8-4.8-33.6-14.4-9.6-8.8-14.4-21.6-14.4-33.6V32l272 272z" fill="#BDEBD7" p-id="2657"></path><path d="M475.2 537.6l-108.8-152h75.2l71.2 108.8 74.4-108.8h72.8l-111.2 152 116.8 161.6h-76L511.2 584l-78.4 116h-74.4l116.8-162.4z" fill="#FFFFFF" p-id="2658"></path></svg>
+
+
 </template>
 
 <script setup>
